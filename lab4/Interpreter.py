eefe6288252aafe26c5fc457ac6a63fe24e1c945
salceson <ciolczyk.michal.93@gmail.com>
2014-12-14 18:31:29 +0100

    Some implementation

--- lab4/Interpreter.py
+++ lab4/Interpreter.py
@@ -17,17 +17,54 @@ class Interpreter(object):
         op = node.op
         return eval("a" + op + "b", {"a": l, "b": r})
 
+    @when(AST.ParenExpr)
+    def visit(self, node):
+        return node.expression.accept(self)
+
     @when(AST.Assignment)
     def visit(self, node):
         pass
 
-    @when(AST.Const)
-    def visit(self, node):
-        return node.value
-
     @when(AST.WhileInstr)
     def visit(self, node):
-        r = None
         while node.cond.accept(self):
-            r = node.body.accept(self)
-        return r
+            try:
+                node.instr.accept(self)
+            except ContinueException:
+                pass
+            except BreakException:
+                break
+
+    @when(AST.RepeatInstr)
+    def visit(self, node):
+        while True:
+            try:
+                node.instrs.accept(self)
+                if node.cond.accept(self):
+                    break
+            except BreakException:
+                break
+            except ContinueException:
+                pass
+
+    @when(AST.IfInstr)
+    def visit(self, node):
+        if node.cond.accept(self):
+            return node.instr.accept(self)
+
+    @when(AST.IfElseInstr)
+    def visit(self, node):
+        if node.cond.accept(self):
+            return node.instr.accept(self)
+        else:
+            return node.elseinstr.accept(self)
+
+    @when(AST.ExprList)
+    def visit(self, node):
+        for expr in node.expr_list:
+            expr.accept(self)
+
+    @when(AST.Instructions)
+    def visit(self, node):
+        for instr in node.instructions:
+            instr.accept(self)

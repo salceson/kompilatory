58f3bed001d93f7608d88c7c31bda1113e1c049f
salceson <ciolczyk.michal.93@gmail.com>
2014-12-14 18:11:54 +0100

    Memory implementation corrections

--- lab4/Memory.py
+++ lab4/Memory.py
@@ -12,45 +12,51 @@ class Memory:
     def put(self, name, value):
         self.memory[name] = value
 
+    def __str__(self):
+        return self.name
+
 
 class NoMemoryError(RuntimeError):
     pass
 
 
 class MemoryStack:
-
     def __init__(self, memory=None):
-        self.memory = memory
         self.stack = []
-        if memory:
-            self.stack.append(memory)
+        self.stack.append(memory if memory else Memory("TopLevelMemory"))
 
     def get(self, name):
-        if self.memory is None:
-            raise NoMemoryError()
-        return self.memory.get(name)
+        for i in xrange(1, len(self.stack)+1):
+            if self.stack[len(self.stack)-i].has_key(name):
+                return self.stack[i].get(name)
+        return None
 
     def insert(self, name, value):
-        if self.memory is not None:
-            self.memory.put(name, value)
+        self.stack[-1].put(name, value)
 
     def set(self, name, value):
-        if self.memory is None:
-            raise NoMemoryError()
-        self.memory.memory[name] = value
+        for i in xrange(1, len(self.stack)+1):
+            if self.stack[len(self.stack)-i].has_key(name):
+                self.stack[i].put(name, value)
+                break
 
     def push(self, memory):
-        self.memory = memory
         self.stack.append(memory)
 
     def pop(self):
         return self.stack.pop()
+
+    def peek(self):
+        return self.stack[-1]
 
 
 if __name__ == "__main__":
     memstack = MemoryStack()
     memory = Memory("test")
     memstack.push(memory)
+    print [str(x) for x in memstack.stack]
+    memstack.insert("troll", 2.0)
+    print memstack.get("troll")
     memstack.set("troll", 10.0)
     print memstack.get("troll")
     d = memstack.get("d")
@@ -59,4 +65,4 @@ if __name__ == "__main__":
     mem = memstack.pop()
     if mem == memory:
         print "OK (mem)"
-    print memstack.stack
+    print [str(x) for x in memstack.stack]
